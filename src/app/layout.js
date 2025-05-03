72c0f558e0120ddac5769c7b34cd17df3c2ef9b2
Md Rakib Hassan <rakib1515hassan@gmail.com>
2025-05-03 17:04:42 +0600

    'update'

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import { ThemeProvider } from "../components/layout/ThemeContext";
 
 const inter = Inter({
